3ae740efbb01d1b2e3ae0546daa945c94bc6904a
hamdi hamza <hamdihamzadev@hotmail.com>
2025-01-06 21:37:33 +0100

    create ThankYouPage

---
+++ src/views/ThankYouPage.vue
@@ -0,0 +1,24 @@
+<template>
+    <section id="ThankYouPage">
+        <b-container>
+            <h4 >'Your order has been placed successfully! 🎉'</h4>
+            <p>Nous apprécions votre confiance. Votre commande est en cours de traitement.</p>
+            <b-row>
+                <b-col>
+                    <b-button>Suivi order</b-button>
+                </b-col>
+                <b-col>
+                    <b-button>Continuer shopping</b-button>
+                </b-col>
+            </b-row>
+        </b-container>
+    </section>
+</template>
+
+<script>
+
+export default{
+    name:'ThankYouPage',
+   
+}
+</script>
